305538621504b193c1fa8b581d346efab5b95b5c
Nathan Fennel <nathanfennel@gmail.com>
2015-08-10 10:35:50 -0400

    Forced frame size on Master Table View

--- Alphabetical List Utility/ALUMasterTableView.m
+++ Alphabetical List Utility/ALUMasterTableView.m
@@ -7,6 +7,9 @@
 //
 
 #import "ALUMasterTableView.h"
+#import "ALUDataManager.h"
+
+static CGFloat const tableViewInset = 0.0f;
 
 @implementation ALUMasterTableView
 
@@ -17,5 +20,26 @@
     // Drawing code
 }
 */
+
+- (void)layoutSubviews {
+	[super layoutSubviews];
+	
+	if (USE_CARDS) {
+		NSArray *sortedIndexPaths = [[self indexPathsForVisibleRows] sortedArrayUsingSelector:@selector(compare:)];
+		for (NSIndexPath *path in sortedIndexPaths) {
+			UITableViewCell *cell = [self cellForRowAtIndexPath:path];
+			[self bringSubviewToFront:cell];
+			[cell.layer setZPosition:path.row];
+		}
+	}
+}
+
+- (CGRect)frame {
+    if (self.superview) {
+        return CGRectOffset(CGRectMake(0.0f, -tableViewInset, self.superview.frame.size.width, self.superview.frame.size.height + tableViewInset + 14.0f), 0.0f, -13.0f);
+    }
+    
+    return CGRectMake(0.0f, 0.0f, 200.0f, 300.0f);
+}
 
 @end
